67e51d00d0da1ab6b71bcbbcad2129072d33ed61
Viktor Georgiev <vgeorgiev1@icloud.com>
2024-04-17 23:20:02 +0300

    progress on mobile version

--- source/index.js
+++ source/index.js
@@ -1,27 +1,33 @@
-window.addEventListener('scroll', function () {
-    let header = document.querySelector('header');
-    let scrollPosition = window.scrollY;
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  let scrollPosition = window.scrollY;
 
-    if (scrollPosition > 400) {
-        header.style.backgroundColor = 'rgba(255, 255, 255, 1)';
-    } else {
-        header.style.backgroundColor = '';
-    }
+  if (scrollPosition > 400) {
+    header.style.backgroundColor = "rgba(255, 255, 255, 1)";
+  } else {
+    header.style.backgroundColor = "";
+  }
 });
 let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
-    centeredSlides: true,
-    initialSlide: 1,
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+  slidesPerView: 2,
+  centeredSlides: true,
+  initialSlide: 1,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
-let map = L.map('map').setView([43.427630, 24.603722], 15);
-let marker = L.marker([43.427630, 24.603722]).addTo(map);
+let map = L.map("map").setView([43.42763, 24.603722], 15);
+let marker = L.marker([43.42763, 24.603722]).addTo(map);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "© OpenStreetMap contributors",
 }).addTo(map);
+
+
+function toggleMenu() {
+  const menu = document.querySelector('.burger-menu');
+  menu.classList.toggle('menu-active');
+}
