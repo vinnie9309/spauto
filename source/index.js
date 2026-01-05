3365a54a4e916a2fadff0d55829d90a060af99c7
vinnie9309 <vgeorgiev1@icloud.com>
2026-01-05 17:09:46 +0200

    design changes

--- source/index.js
+++ source/index.js
@@ -2,6 +2,7 @@ let header = document.querySelector("header");
 let nav = document.querySelector(".nav-links");
 let menu = document.querySelector(".burger-menu");
 
+
 window.addEventListener("scroll", function () {
   if (window.scrollY > 400 && window.innerWidth > 768) {
     header.style.backgroundColor = "rgba(255, 255, 255, 1)";
@@ -9,6 +10,26 @@ window.addEventListener("scroll", function () {
     header.style.backgroundColor = "";
   }
 });
+const dropdown = document.querySelector(".dropdown");
+const dropdownLink = dropdown?.querySelector(".dropdown-link");
+const dropdownMenu = dropdown?.querySelector(".dropdown-menu")
+if (dropdown && dropdownLink) {
+  dropdownLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    dropdown.classList.toggle("open");
+    dropdownMenu.classList.toggle("show")
+  });
+
+  document.addEventListener("click", (e) => {
+    console.log('ere')
+    if (!dropdown.contains(e.target)) {
+      console.log('a')
+      dropdown.classList.remove("open");
+      dropdownMenu.classList.remove("show")
+    }
+  });
+}
 let swiper = new Swiper(".mySwiper", {
   slidesPerView: 2,
   centeredSlides: true,
@@ -34,6 +55,7 @@ function toggleMenu() {
   menu.classList.toggle("menu-active");
   nav.style.display = nav.style.display === "flex" ? "none" : "flex";
 }
+
 if(window.innerWidth < 768) {
   document.addEventListener("click", function (e) {
     if (!e.target.classList.contains("line") && !e.target.classList.contains("burger-menu")) {
